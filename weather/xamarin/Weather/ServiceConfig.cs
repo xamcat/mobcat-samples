7e6fc72343622cd4cf504d0114b51401a1339003
Sweekriti Satpathy <swsat@microsoft.com>
2020-04-17 12:40:27 -0700

    adds back missing Service.Config to weather

---
+++ weather/xamarin/Weather/ServiceConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Weather
+{
+    public class ServiceConfig
+    {
+
+        public const string AndroidAppCenterSecret = "";
+        public const string iOSAppCenterSecret = "";
+        public const string WeatherServiceUrl = "";
+        public const string WeatherServiceApiKey = "";
+    }
+}
