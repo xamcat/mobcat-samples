0894304a698fde4afbb901103edbfc2f7405a365
Sweekriti Satpathy <swsat@microsoft.com>
2020-05-25 18:13:16 -0700

    adds missing service config file back

---
+++ weather/xamarin/Weather/ServiceConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Weather
+{
+    /// <summary>
+    /// Secret key file.
+    /// Replace these values with your API Key values
+    /// </summary>
+    public static class ServiceConfig
+    {
+        public static readonly string WeatherServiceApiKey = "";
+
+        public static readonly string WeatherServiceUrl = "";
+
+        public static readonly string AndroidAppCenterSecret = "";
+
+        public static readonly string iOSAppCenterSecret = "";
+    }
+}
