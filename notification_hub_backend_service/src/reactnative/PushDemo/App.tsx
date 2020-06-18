dad1c1900b58699be43b2ced1fa125ee010e844e
Alexey Strakh <alexey.strakh@microsoft.com>
2020-06-17 18:45:52 -0700

    ios and android push notifications registrations and connection with the azure rest api

--- notification_hub_backend_service/src/reactnative/PushDemo/App.tsx
+++ notification_hub_backend_service/src/reactnative/PushDemo/App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,6 +16,7 @@ import {
   View,
   Text,
   StatusBar,
+  Alert,
 } from 'react-native';
 
 import {
@@ -25,23 +26,89 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import DemoNotificationService from './DemoNotificationService';
 
 declare const global: {HermesInternal: null | {}};
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <Text>PushDemo is a React Native application which registers for push notifications with Azure Web API backend and subscribes to receive push updates</Text>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+class App extends Component {
+  private notificationService: DemoNotificationService;
+
+  constructor(props) {
+    console.log('!!! App.ctr');
+    super(props);
+    this.state = {};
+
+    this.notificationService = new DemoNotificationService(
+      this.onRegister.bind(this),
+      this.onNotification.bind(this),
+    );
+
+    console.log('!!! App.ctr completed');
+  }
+
+  render(){
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            <Text>PushDemo is a React Native application which registers for push notifications with Azure Web API backend and subscribes to receive push updates</Text>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+
+  async onRegister(token) {
+    console.log('!!! App.onRegister');
+    Alert.alert("Registered", `The app has been successfully registered for push notifications: ${token.os}=${token.token}`);
+    this.setState({registerToken: token.token, fcmRegistered: true});
+
+    const registerApiUrl = "https://push-demo-api-alstrakh.azurewebsites.net/api/notifications/installations";
+    const deviceId = "A556CF39-8A55-4F7E-9DE3-E5863FAAF8BB";
+    const pnPlatform = token.os == "ios" ? "apns" : "fcm";
+    const pnGenericTemplate = token.os == "ios" ? "{\"aps\":{\"alert\":\"$(alertMessage)\"}, \"action\": \"$(alertAction)\"}" : "{\"data\":{\"message\":\"$(alertMessage)\", \"action\":\"$(alertAction)\"}}";
+    const pnSilentTemplate = token.os == "ios" ? "{\"aps\":{\"content-available\":1, \"apns-priority\": 5, \"sound\":\"\", \"badge\": 0}, \"message\": \"$(silentMessage)\", \"action\": \"$(silentAction)\"}": "{\"data\":{\"message\":\"$(silentMessage)\", \"action\":\"$(silentAction)\", \"silent\":\"true\"}}"
+    const apiKey = '123-456';
+      
+    const result = await fetch(registerApiUrl, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'apiKey': apiKey
+      },
+      body: JSON.stringify({
+        installationId: deviceId,
+        platform: pnPlatform,
+        pushChannel: token.token,
+        tags: [],
+        templates: {
+            genericTemplate: {
+                body: pnGenericTemplate
+            },
+            silentTemplate: {
+                body: pnSilentTemplate
+            }
+        }
+      })
+    });
+
+    console.log(result);
+  }
+
+  onNotification(notification) {
+    console.log('!!! App.onNotification');
+    Alert.alert(notification.title, notification.message);
+  }
+
+  handlePerm(permissions) {
+    console.log('!!! App.handlePerm');
+    Alert.alert('Permissions', JSON.stringify(perms));
+  }
 };
 
 const styles = StyleSheet.create({
--- notification_hub_backend_service/src/reactnative/PushDemo/android/app/src/main/java/com/pushdemo/MainApplication.java
+++ notification_hub_backend_service/src/reactnative/PushDemo/android/app/src/main/java/com/pushdemo/MainApplication.java
@@ -2,6 +2,8 @@ package com.pushdemo;
 
 import android.app.Application;
 import android.content.Context;
+
+import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
