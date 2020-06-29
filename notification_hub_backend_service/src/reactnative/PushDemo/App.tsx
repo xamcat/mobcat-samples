4ee499679c94ebb7fd749425deefe571a5760ae4
Alexey Strakh <alexey.strakh@microsoft.com>
2020-06-29 14:09:02 -0700

    add handling for non silent notifications and remote templates from the client

--- notification_hub_backend_service/src/reactnative/PushDemo/App.tsx
+++ notification_hub_backend_service/src/reactnative/PushDemo/App.tsx
@@ -80,21 +80,11 @@ class App extends Component<IState> {
       this.setState({ isBusy: true, status: status });
       const pnPlatform = this.state.registeredOS == "ios" ? "apns" : "fcm";
       const pnToken = this.state.registerToken;
-      const pnGenericTemplate = this.state.registeredOS == "ios" ? "{\"aps\":{\"alert\":\"$(alertMessage)\"}, \"action\": \"$(alertAction)\"}" : "{\"data\":{\"message\":\"$(alertMessage)\", \"action\":\"$(alertAction)\"}}";
-      const pnSilentTemplate = this.state.registeredOS == "ios" ? "{\"aps\":{\"content-available\":1, \"apns-priority\": 5, \"sound\":\"\", \"badge\": 0}, \"message\": \"$(silentMessage)\", \"action\": \"$(silentAction)\"}" : "{\"data\":{\"message\":\"$(silentMessage)\", \"action\":\"$(silentAction)\", \"silent\":\"true\"}}"
       const request = {
         installationId: this.deviceId,
         platform: pnPlatform,
         pushChannel: pnToken,
-        tags: [],
-        templates: {
-          genericTemplate: {
-            body: pnGenericTemplate
-          },
-          silentTemplate: {
-            body: pnSilentTemplate
-          }
-        }
+        tags: []
       };
       const response = await this.notificationRegistrationService.registerAsync(request);
       status = `Registered for ${this.state.registeredOS} push notifications`;
@@ -128,8 +118,12 @@ class App extends Component<IState> {
   }
 
   onNotification(notification: any) {
+    console.log(`Received a push notification on ${this.state.registeredOS}`);
     this.setState({ status: `Received a push notification...` });
-    Alert.alert(notification.data.action, notification.data.message);
+
+    if (notification.data.message) {
+      Alert.alert(notification.data.action, notification.data.message);
+    }
   }
 };
 
