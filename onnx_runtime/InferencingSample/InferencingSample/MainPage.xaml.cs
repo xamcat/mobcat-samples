39b27ed0eb0f06df20a54d2d058afc22d9ef71bd
Mike Parker <mikeparker104@users.noreply.github.com>
2021-12-02 12:11:15 +0000

    Basic sample demonstrating use of ONNX Runtime in a Xamarin.Forms app (#41)

---
+++ onnx_runtime/InferencingSample/InferencingSample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InferencingSample
+{
+    public partial class MainPage : ContentPage
+    {
+        MobileNetImageClassifier _classifier;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _classifier = new MobileNetImageClassifier();
+        }
+
+        async Task RunInferenceAsync()
+        {
+            RunButton.IsEnabled = false;
+
+            try
+            {
+                var sampleImage = await _classifier.GetSampleImageAsync();
+                var result = await _classifier.GetClassificationAsync(sampleImage);
+                await DisplayAlert("Result", result, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                RunButton.IsEnabled = true;
+            }
+        }
+
+        void RunButton_Clicked(object sender, EventArgs e)
+            => _ = RunInferenceAsync();
+    }
+}
